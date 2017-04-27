3a14b5833fb8e4fdc4dd8be8ef064957264a0532
Matthew Bardoe <mbardoe@gmail.com>
2017-04-26 20:43:09 -0400

    Nice clean up. Things working well.

--- surrealnumber.py
+++ surrealnumber.py
@@ -15,27 +15,45 @@ class SurrealNumber(object):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    @property
-    def left(self):
-        return self.left
-
-    @property
-    def right(self):
-        return self.right
-
-
     def __add__(self, other):
         if isinstance(other, SurrealNumber):
-            myleft=self.get_left
-            otherleft=other.get_left
+            my_left = self.left
+            other_left = other.left
+            new_left = [self + n for n in other_left] + [other + n for n in
+                                                       my_left]
+            my_right = self.right
+            other_right = other.right
+            new_right = [self + n for n in other_right] + [other + n for n in
+                                                         my_right]
+            return SurrealNumber(left=new_left, right=new_right)
+        else:
+            raise TypeError(
+                'Must add surreal numbers to other surreal numbers.')
 
     def __repr__(self):
-        ans='{'
+        ans = '{'
         for i in self.left:
-            ans+=' '+str(i)+','
-        ans=ans[:-1]
-        ans+=' | '
-        for i in self.left:
-            ans+=' '+str(i)+','
-        return ans[:-1]+'}'
+            ans += ' ' + str(i) + ','
+        if len(self.left) > 0:
+            ans = ans[:-1]
+        ans += ' | '
+        for i in self.right:
+            ans += ' ' + str(i) + ','
+        if len(self.right) > 0:
+            ans = ans[:-1]
+        return ans + '}'
 
+
+def main():
+    zero = SurrealNumber(left=[], right=[])
+    one = SurrealNumber(left=[zero], right=[])
+    neg_one = SurrealNumber(left=[], right=[zero])
+    star = SurrealNumber(left=[zero], right=[zero])
+
+    print(str(zero))
+    print(str(one))
+    print(str(one + one))
+
+
+if __name__ == '__main__':
+    main()
