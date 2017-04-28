99622fdd5d1ca1adb0e3acf8909d6fb3b384eecd
Matthew Bardoe <mbardoe@gmail.com>
2017-04-28 17:33:00 -0400

    Many updates to the surreal number class, but it running very slowly on nimbers. We may need a way to increase the computability of nimbers.

--- surrealnumber.py
+++ surrealnumber.py
@@ -21,10 +21,10 @@ class SurrealNumber(object):
     '''
 
     def __init__(self, **kwargs):
-        try:
-            setattr(self, '_value', kwargs.pop('value'))
-        except:
-            pass
+        # try:
+        #     setattr(self, '_value', kwargs.pop('value'))
+        # except:
+        #     pass
         try:
             setattr(self, '_left', kwargs.pop('left'))
         except:
@@ -38,6 +38,8 @@ class SurrealNumber(object):
 
     @property
     def value(self):
+        if not self.is_number():
+            raise TypeError("Only numbers have a value")
         #self.simplify()
         if len(self.left) == 0 and len(self.right) == 0:
             return 0
@@ -97,6 +99,17 @@ class SurrealNumber(object):
                             guess = guess + step
                     return sign * guess
 
+
+    @property
+    def nim_value(self):
+        if not self.is_nimber():
+            raise TypeError("Only nimbers have a nim_value")
+        n=0
+        while (SurrealNumber.nimber(n) in self.right):
+            n+=1
+            #print(n)
+        return n
+
     @property
     def left(self):
         return self._left
@@ -111,18 +124,20 @@ class SurrealNumber(object):
 
     def __add__(self, other):
         if isinstance(other, SurrealNumber):
+            if self.is_nimber() and other.is_nimber():
+                return SurrealNumber.nimber(self.nim_value^other.nim_value)
             my_left = self.left
             other_left = other.left
             new_left = [self + n for n in other_left] + [other + n for n in
-                                                       my_left]
+                                                         my_left]
             #print(new_left)
             my_right = self.right
             other_right = other.right
             new_right = [self + n for n in other_right] + [other + n for n in
-                                                         my_right]
+                                                           my_right]
             #print(new_right)
-            n=SurrealNumber(left=new_left, right=new_right)
-            n.simplify()
+            n = SurrealNumber(left=new_left, right=new_right)
+            #n.simplify()
             return n
         else:
             raise TypeError(
@@ -134,58 +149,126 @@ class SurrealNumber(object):
             other_left = other.left
             my_right = self.right
             other_right = other.right
-            new_left = [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_left, other_left)] +\
-                        [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_right, other_right)]
+            new_left = [n[0] * other + self * n[1] - n[0] * n[1] for n in
+                        itertools.product(my_left, other_left)] + \
+                       [n[0] * other + self * n[1] - n[0] * n[1] for n in
+                        itertools.product(my_right, other_right)]
 
-            new_right = [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_left, other_right)]+\
-                        [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_left, other_left)]
-            return SurrealNumber(left=new_left, right=new_right)
+            new_right = [n[0] * other + self * n[1] - n[0] * n[1] for n in
+                         itertools.product(my_left, other_right)] + \
+                        [n[0] * other + self * n[1] - n[0] * n[1] for n in
+                         itertools.product(my_left, other_left)]
+            n = SurrealNumber(left=new_left, right=new_right)
+            n.simplify()
+            return n
         else:
             raise TypeError(
                 'Must multiply surreal numbers to other surreal numbers.')
 
     def simplify(self):
         for n in self.left:
-            n=n.simplify()
+            n = n.simplify()
         for n in self.right:
-            n=n.simplify()
+            n = n.simplify()
+        oldright=list(self.right)
+        oldleft=list(self.left)
         self.remove_dominated_options()
+        self.remove_reversible_options()
+        while((not oldleft==self.left) and (not oldright==self.right)):
+            self.remove_dominated_options()
+            self.remove_reversible_options()
+            oldright=list(self.right)
+            oldleft=list(self.left)
 
 
     def remove_dominated_options(self):
-        x=[[n<=m for m in self.right] for n in self.right]
-        y=[functools.reduce(operator.mul, y, 1) for y in x]
-        right=list(itertools.compress(self.right,y))
-        new_right=[]
-        for n in right:
-            if not n in new_right:
-                new_right.append(n)
-        self._right=new_right
-        x=[[n>=m for m in self.left] for n in self.left]
-        y=[functools.reduce(operator.mul, y, 1) for y in x]
-        left=list(itertools.compress(self.left,y))
-        new_left=[]
-        for n in left:
-            if not n in new_left:
-                new_left.append(n)
-        self._left=new_left
+        x=self.dominated_right_option()
+        while x:
+            self._right=[n for n in self.right if n!=x]
+            x=self.dominated_right_option()
+        x=self.dominated_left_option()
+        while x:
+            self._right=[n for n in self.left if n!=x]
+            x=self.dominated_left_option()
+
+    def dominated_right_option(self):
+        testlist=itertools.combinations(self.right,2)
+        for a,b in testlist:
+            if a<=b:
+                return b
+        return None
+
+    def dominated_left_option(self):
+        testlist=itertools.combinations(self.right,2)
+        for a,b in testlist:
+            if a<=b:
+                return a
+        return None
+
+    def remove_reversible_options(self):
+        left_remove = []
+        left_new = []
+        right_remove = []
+        right_new = []
+        for n in self.right:
+            for j in n.left:
+                if j >= self:
+                    #j is a reversible right option.
+                    #print("found one")
+                    right_remove.append(n)
+                    right_new += j.right
+        for n in self.left:
+            for j in n.right:
+                if j <= self:
+                    #j is a reversible left option
+                    #print("found one")
+                    left_remove.append(n)
+                    left_new += j.left
+        self._right = [n for n in self.right if
+                       n not in right_remove]
+        for n in right_new:
+            if n not in self.right:
+                self._right.append(n)
+
+        self._left = [n for n in self.left if n not in left_remove]
+        for n in left_new:
+            if n not in self.left:
+                self._left.append(n)
+
 
     def is_number(self):
-        test=itertools.product(self.right,self.left)
-        return bool(functools.reduce(operator.mul, [i[0]<i[1] for i in test], 1))
+        test = itertools.product(self.right, self.left)
+        for a,b in test:
+            if not a<b:
+                return False
+        return True
+
+    def is_nimber(self):
+        if self.right != self.left:
+            return False
+        for n in self.right:
+            if not n.is_nimber():
+                return False
+        return True
 
     def __repr__(self):
+        if self.is_nimber():
+            try:
+                return str('*'+str(self.nim_value))
+            except:
+                pass
         if self.is_number():
             try:
                 return str(self.value)
             except:
                 pass
+
         ans = '{'
         for i in self.left:
             ans += ' ' + str(i) + ','
         if len(self.left) > 0:
             ans = ans[:-1]
-        ans += ' | '
+        ans += ' |'
         for i in self.right:
             ans += ' ' + str(i) + ','
         if len(self.right) > 0:
@@ -198,6 +281,11 @@ class SurrealNumber(object):
                 return str(self.value)
             except:
                 pass
+        if self.is_nimber():
+            try:
+                return str('*'+str(self.nim_value))
+            except:
+                pass
         return self.__repr__()
 
     def __lt__(self, other):
@@ -208,45 +296,66 @@ class SurrealNumber(object):
 
     def __le__(self, other):
         for n in self.left:
-            if  other<=n:
+            if other <= n:
                 return False
         for n in other.right:
-            if  n<=self:
+            if n <= self:
                 return False
         return True
 
     def __ge__(self, other):
         for n in self.right:
-            if n<=other:
+            if n <= other:
                 return False
         for n in other.left:
-            if self<=n:
+            if self <= n:
                 return False
         return True
 
     def __eq__(self, other):
         self.simplify()
         other.simplify()
-        return (self.left==other.left) and (self.right==other.right)
+        return (self.left == other.left) and (self.right == other.right)
 
     def __neg__(self):
-        new_left=[-n for n in self.right]
-        new_right=[-n for n in self.left]
+        new_left = [-n for n in self.right]
+        new_right = [-n for n in self.left]
         return SurrealNumber(left=new_left, right=new_right)
 
     @classmethod
-    def create_integer(cls, n):
+    def integer(cls, n):
         if not isinstance(n, int):
             raise TypeError("Function requires an integer")
         if n == 0:
-            return cls(left=[], right=[], value=0)
+            return cls(left=[], right=[])
         elif n > 0:
-            return cls(left=[cls.create_integer(n - 1)], right=[], value=n)
+            return cls(left=[cls.integer(n - 1)], right=[])
         else:
-            return cls(left=[], right=[cls.create_integer(n + 1)], value=n)
+            return cls(left=[], right=[cls.integer(n + 1)])
 
     @classmethod
-    def create_fraction(cls, f):
+    def nimber(cls, n):
+        if not isinstance(n, int):
+            raise TypeError("Function requires an integer")
+        options = []
+        for i in range(n):
+            options.append(SurrealNumber.nimber(i))
+        return cls(left=options, right=options)
+
+    @classmethod
+    def ups(cls, n):
+        if not isinstance(n, int):
+            raise TypeError("Function requires an integer")
+        zero=cls.integer(0)
+        ans=cls.integer(0)
+        up=cls(left=[zero], right=[cls.nimber(1)])
+        for i in range(n):
+            ans=ans+up
+        ans.simplify()
+        return ans
+
+    @classmethod
+    def fraction(cls, f):
         if not isinstance(f, Fraction):
             raise TypeError("Function requires an fraction")
         denominator = f.denominator
@@ -265,7 +374,6 @@ class SurrealNumber(object):
         return cls(left=[leftnum], right=[rightnum])
 
 
-
 def main():
     zero2 = SurrealNumber(value=0)
     zero = SurrealNumber(left=[], right=[])
@@ -275,26 +383,24 @@ def main():
     one = SurrealNumber(left=[zero], right=[])
     neg_one = SurrealNumber(left=[], right=[zero])
     star = SurrealNumber(left=[zero], right=[zero])
-    switch=SurrealNumber(left=[one], right=[neg_one])
+    switch = SurrealNumber(left=[one], right=[neg_one])
 
-    print("zero = "+str(zero))
-    print("one = "+ str(one))
-    print(one.left[0]==zero)
-    print(zero>=one)
+    print("zero = " + str(zero))
+    print("one = " + str(one))
+    print(one.left[0] == zero)
+    print(zero >= one)
     print(star)
     print(star.is_number())
     print(neg_one)
     print(switch)
     two = one + one
     two.simplify()
-    print (two.left)
-    print (two+two+two)
+    print(two.left)
+    print(two + two + two)
     print("one plus one")
     print(str(one + one))
-    print(two + star+switch)
+    print(two + star + switch)
     print(star + star)
-
-
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -4,28 +4,52 @@ import unittest
 
 
 class TestNumbers(unittest.TestCase):
-    one = Number(value=1)
-    negative_two = Number(value=-2)
-    three = Number(value=3)
-    zero = Number(value=0)
-    twelve = Number(value=12)
+    one = SurrealNumber.integer(1)
+    negative_two = SurrealNumber.create_integer(-2)
+    three = SurrealNumber.integer(3)
+    zero = SurrealNumber.integer(0)
+    twelve = SurrealNumber.integer(12)
+    star = SurrealNumber(left=[zero], right=[0])
 
     def test_create_number(self):
-        self.assertIsInstance(TestNumbers.three, Number)
-        self.assertIsInstance(Number(left=[TestNumbers.negative_two],
-                                     right=[TestNumbers.one]),
-                              Number)
-        self.assertIsInstance(Number(value=3), Number)
+        self.assertIsInstance(TestNumbers.three, SurrealNumber)
+        self.assertIsInstance(SurrealNumber(left=[TestNumbers.negative_two],
+                                            right=[TestNumbers.one]),
+                              SurrealNumber)
+        self.assertIsInstance(SurrealNumber(value=3), SurrealNumber)
         x = TestNumbers.one + TestNumbers.three
-        self.assertIsInstance(x, Number)
+        self.assertIsInstance(x, SurrealNumber)
 
-    def test_simplest_number(self):
-        self.assertEqual(Number.simplest_between(TestNumbers.one,
-                                                 TestNumbers.three),
-                         Number(value=2))
-        self.assertEqual(Number.simplest_between(TestNumbers.negative_two,
-                                                 TestNumbers.twelve),
-                         Number(value=0))
+    # def test_simplest_number(self):
+    #     self.assertEqual(Number.simplest_between(TestNumbers.one,
+    #                                              TestNumbers.three),
+    #                      Number(value=2))
+    #     self.assertEqual(Number.simplest_between(TestNumbers.negative_two,
+    #                                              TestNumbers.twelve),
+    #                      Number(value=0))
+
+    def test_number_identification(self):
+        self.assertEqual(str(TestNumbers.star), '{ 0 |  0 }')
+        self.assertEqual(str(TestNumbers.twelve), '12')
+        self.assertEqual(str(TestNumbers.one + TestNumbers.twelve), '13')
+        self.assertEqual(str(TestNumbers.one + TestNumbers.star), '{ 1 | 1 }')
+        self.assertFalse(TestNumbers.star.is_number())
+        self.assertTrue(TestNumbers.twelve.is_number())
+
+
+    def test_number_summation(self):
+        self.assertEqual(TestNumbers.one + TestNumbers.one,
+                         SurrealNumber(value=2))
+        self.assertEqual(TestNumbers.star+TestNumbers.three,
+                         SurrealNumber(left=[TestNumbers.three],
+                                       right=[TestNumbers.three]))
+        self.assertEqual(TestNumbers.star+TestNumbers.star,TestNumbers.zero)
+
+    def test_number_ordering(self):
+        self.assertFalse(TestNumbers.three < TestNumbers.zero)
+        self.assertFalse(TestNumbers.star < TestNumbers.zero)
+        self.assertTrue(
+            SurrealNumber(left=[TestNumbers.zero], right=[]) == TestNumbers.one)
 
 
 if __name__ == "__main__":
