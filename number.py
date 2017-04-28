25a242ba2612bf8ae9424c545c5e37f934311c4a
Matthew Bardoe <mbardoe@gmail.com>
2017-04-28 11:03:47 -0400

    Added comparison operators for Number, how to compute the value of a Number, comparison, negation for surreals, and removal of dominated positions.

--- number.py
+++ number.py
@@ -1,4 +1,7 @@
 import surrealnumber
+from fractions import Fraction
+
+
 class Number(surrealnumber.SurrealNumber):
     '''A class that represents a standard number within the surreal numbers.
     Right now we will focus on numbers that were born on a finite day.
@@ -14,56 +17,134 @@ class Number(surrealnumber.SurrealNumber):
 
     def __init__(self, **kwargs):
         if 'value' in kwargs.keys():
-            n=kwargs['value']
-            if n==int(n):
-                if n==0:
-                    kwargs['left']=[]
-                    kwargs['right']=[]
-                if n>0:
-                    kwargs['left']=[Number(value=n-1)]
-                    kwargs['right']=[]
-                if n<0:
-                    kwargs['left']=[]
-                    kwargs['right']=[Number(value=n+1)]
+            n = kwargs['value']
+            if n == int(n):
+                if n == 0:
+                    kwargs['left'] = []
+                    kwargs['right'] = []
+                if n > 0:
+                    kwargs['left'] = [Number(value=n - 1)]
+                    kwargs['right'] = []
+                if n < 0:
+                    kwargs['left'] = []
+                    kwargs['right'] = [Number(value=n + 1)]
             else:
                 raise ValueError('no non-integers yet.')
-
-
         super().__init__(**kwargs)
 
     def __add__(self, other):
-        if isinstance(other,Number):
-            new_value=self.value+other.value
+        if isinstance(other, Number):
+            new_value = self.value + other.value
             return Number(value=new_value)
 
     def __repr__(self):
-        return str(self.value)
+        try:
+            return str(self.value)
+        except AttributeError:
+            return str(super().__repr__())
+
 
     def __le__(self, other):
         if isinstance(other, Number):
-            return self.value<other.value
+            return self.value < other.value
         else:
             return super().__le__(other)
 
     def __ge__(self, other):
         if isinstance(other, Number):
-            return self.value>other.value
+            return self.value > other.value
         else:
             return super().__ge__(other)
 
     def __eq__(self, other):
         if isinstance(other, Number):
-            return self.value==other.value
+            try:
+                return self.value == other.value
+            except AttributeError:
+                return self.find_value() == other.find_value()
+            finally:
+                raise SyntaxError ("something went wrong in finding the value of these numbers")
         else:
             return super().__eq__(other)
-        return False
 
     @classmethod
-    def createNumber(cls,left,right):
+    def create_number(cls, left, right):
         pass
 
+    def find_value(self):
+        self.simplify()
+        if len(self.left) == 0 and len(self.right) == 0:
+            return 0
+        elif len(self.left) == 1 and len(self.right) == 0:
+            try:
+                n = self.left[0].find_value
+            except Exception as e:
+                print(e)
+                return None
+            if n >= 0:
+                return int(n) + 1
+            if n < 0:
+                return 0
+        if len(self.right) == 1 and len(self.left) == 0:
+            try:
+                n = self.right[0].find_value
+            except Exception as e:
+                print(e)
+                return None
+            if n <= 0:
+                return int(n) - 1
+            if n > 0:
+                return 0
+        if len(self.right) == 1 and len(self.left) == 1:
+            try:
+                r = self.right[0].find_value
+                l = self.left[0].find_value
+            except Exception as e:
+                print(e)
+                return None
+
+            if l < r:
+                if r > 0 and l < 0:
+                    return 0
+                if r < 0 and l < 0:
+                    sign = -1
+                    r = -1 * l
+                    r = -1 * r
+                else:
+                    sign = 1
+                if (r - l) > 1:
+                    return int(l) + 1
+                else:
+                    current = 1
+                    denom = 2 ** (current)
+                    step = Fraction(1, denom)
+                    #guess =int(left)+power
+                    guess = int(l) + step
+                    while (l >= guess):
+                        #print guess
+                        if int(guess + step) >= int(r):
+                            current += 1
+                            denom = 2 ** (current)
+                            step = Fraction(1, denom)
+                            guess = int(l) + step
+                        else:
+                            guess = guess + step
+                    return sign * guess
+
+
+    @classmethod
+    def create_integer(cls, n):
+        if not isinstance(n, int):
+            raise TypeError("Function requires an integer")
+        if n == 0:
+            return cls(left=[], right=[], value=0)
+        elif n > 0:
+            return cls(left=[cls.create_integer(n - 1)], right=[], value=n)
+        else:
+            return cls(left=[], right=[cls.create_integer(n + 1)], value=n)
+
     @staticmethod
-    def simplest_between(left,right):
+    def simplest_between(left, right):
         """Calculates the simplest number between two given numbers.
 
         Args:
@@ -76,32 +157,32 @@ class Number(surrealnumber.SurrealNumber):
         if not isinstance(left, Number) or not isinstance(right, Number):
             raise ValueError('One of these is not a Number')
         left = left.value
-        right= right.value
-        if left>right:
-            raise ValueError ('Only appropriate for Numbers')
+        right = right.value
+        if left > right:
+            raise ValueError('Only appropriate for Numbers')
         #print str(left) + ' ' +str(right)
         if left is None:
             if right is None:
                 return Number(value=0)
-            if right<=0:
-                return Number(value = int(right)-1)
+            if right <= 0:
+                return Number(value=int(right) - 1)
             else:
                 return Number(value=0)
         if right is None:
             if left >= 0:
-                return Number(value = int(left)+1)
+                return Number(value=int(left) + 1)
             else:
                 return Number(value=0)
-        if left<Number(value=0) and right>Number(value=0):
+        if left < Number(value=0) and right > Number(value=0):
             return Number(value=0)
 
-        if right<Number(value=0):
-            right,left=-1*left,-1*right
-            sign=-1
+        if right < Number(value=0):
+            right, left = -1 * left, -1 * right
+            sign = -1
         else:
-            sign=1
-        if int(right)-int(left)>1:
-            return Number(value = sign*(int(left)+1))
+            sign = 1
+        if int(right) - int(left) > 1:
+            return Number(value=sign * (int(left) + 1))
         else:
             raise ValueError("Haven't implemented fractions")
             # current=1
@@ -120,3 +201,19 @@ class Number(surrealnumber.SurrealNumber):
 
     def __int__(self):
         return Number(value=int(self.value))
+
+
+def main():
+    zero = Number(left=[], right=[])
+    print(zero.find_value())
+    #one = SurrealNumber(left=[zero], right=[])
+    #neg_one = SurrealNumber(left=[], right=[zero])
+    #star = SurrealNumber(left=[zero], right=[zero])
+
+    print(str(zero))
+    #print(str(one))
+    #print(str(one + one))
+
+
+if __name__ == '__main__':
+    main()
--- surrealnumber.py
+++ surrealnumber.py
@@ -1,3 +1,8 @@
+import operator
+import functools
+import itertools
+
+
 class SurrealNumber(object):
     '''A class that represents a surreal number. Right now we will focus on
     surreal numbers that were born on a finite day.
@@ -30,6 +35,41 @@ class SurrealNumber(object):
             raise TypeError(
                 'Must add surreal numbers to other surreal numbers.')
 
+    def __mul__(self, other):
+        if isinstance(other, SurrealNumber):
+            my_left = self.left
+            other_left = other.left
+            my_right = self.right
+            other_right = other.right
+            new_left = [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_left, other_left)] +\
+                        [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_right, other_right)]
+
+            new_right = [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_left, other_right)]+\
+                        [n[0]*other + self*n[1] - n[0]*n[1] for n in itertools.product(my_left, other_left)]
+            return SurrealNumber(left=new_left, right=new_right)
+        else:
+            raise TypeError(
+                'Must multiply surreal numbers to other surreal numbers.')
+    def simplify(self):
+        for n in self.left:
+            n=n.simplify
+        for n in self.right:
+            n=n.simplify
+        self.remove_dominated_options()
+
+
+    def remove_dominated_options(self):
+        x=[[n<=m for m in self.right] for n in self.right]
+        y=[functools.reduce(operator.mul, y, 1) for y in x]
+        self.right=list(itertools.compress(self.right,y))
+        x=[[n>=m for m in self.left] for n in self.left]
+        y=[functools.reduce(operator.mul, y, 1) for y in x]
+        self.left=list(itertools.compress(self.left,y))
+
+    def is_number(self):
+        test=itertools.product(self.right,self.left)
+        return bool(functools.reduce(operator.mul, [i[0]<i[1] for i in test], 1))
+
     def __repr__(self):
         ans = '{'
         for i in self.left:
@@ -42,6 +82,37 @@ class SurrealNumber(object):
         if len(self.right) > 0:
             ans = ans[:-1]
         return ans + '}'
+
+    def __le__(self, other):
+        for n in self.right:
+            if not n<other:
+                return False
+        for n in other.left:
+            if not self<n:
+                return False
+        return True
+
+    def __ge__(self, other):
+        for n in self.right:
+            if not n>other:
+                return False
+        for n in other.left:
+            if not self>n:
+                return False
+        return True
+
+    def __eq__(self, other):
+        self.simplify()
+        other.simplify()
+        return (self.left==other.left) and (self.right==other.right)
+
+    def __neg__(self):
+        new_left=[-n for n in self.right]
+        new_right=[-n for n in self.left]
+        return SurrealNumber(left=new_left, right=new_right)
+
+
+
 
 
 def main():
